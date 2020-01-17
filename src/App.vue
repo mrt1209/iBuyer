86301450bd7f2172446e2750660b5eeb90341f4d
mrt1209 <mrt1209@gmail.com>
2020-01-16 23:13:05 -0500

    First commit after setup

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+    <div id="app">
+        <div id="nav">
+            <router-link to="/">Home</router-link>|
+            <router-link to="/about">About</router-link>
+        </div>
+        <router-view />
+        <div>
+            <b-button variant="primary">Primary</b-button>
+            <b-button variant="secondary">Secondary</b-button>
+            <b-button variant="success">Success</b-button>
+            <b-button variant="danger">Danger</b-button>
+            <b-button variant="warning">Warning</b-button>
+            <b-button variant="info">Info</b-button>
+            <b-button variant="light">Light</b-button>
+            <b-button variant="dark">Dark</b-button>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+import { BButton } from "bootstrap-vue";
+
+@Component({
+    components: {
+        BButton
+    }
+})
+export default class App extends Vue {
+  // @Prop()
+  // private displaySingleColumnList!: boolean;
+}
+</script>
+
+<style>
+#app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+}
+
+#nav {
+    padding: 30px;
+}
+
+#nav a {
+    font-weight: bold;
+    color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+    color: #42b983;
+}
+</style>
